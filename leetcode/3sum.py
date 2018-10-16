22f6da874e40fca87e12dfd76e96db239873e1aa
Beannn <450728009@QQ.COM>
2018-10-16 21:56:18 +0800

    增加第三种方法

--- leetcode/3sum.py
+++ leetcode/3sum.py
@@ -61,3 +61,32 @@ class Solution(object):
                             res.append([i, mid, j])
             return res
 
+class Solution:
+    def threeSum(self,nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res= []
+        i = 0
+        while i < len(nums) - 2:
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    res.append([nums[i],nums[j] , nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k+1]:
+                        k -= 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    j += 1
+            i += 1
+            while i < len(nums) - 2 and nums[i] == nums[i-1]:
+                i += 1
+        return res
